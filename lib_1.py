5fd2cfb49935ea6db44fa247a394a8e80d56a14f
Jakub Wieczorek <qubawie@gmail.com>
2025-11-16 22:26:12 +0100

    first commit

---
+++ lib_1.py
@@ -0,0 +1,8 @@
+def arithemtic_mean(data):
+    #print("data type", type(data))
+    return sum(data) / len(data)
+def test_function():
+    print("test")
+    
+#if __name__ == "__main__":
+    #pass
---
+++ main.py
@@ -0,0 +1,5 @@
+import lib_1 as lib
+
+list = [1, 2]
+
+print(lib.arithemtic_mean(list))
