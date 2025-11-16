d8b9cbe93552386862c9b9dbe59650a5619b61af
Jakub Wieczorek <qubawie@gmail.com>
2025-11-16 23:23:47 +0100

    working on Mode

--- lib_1.py
+++ lib_1.py
@@ -1,8 +1,34 @@
+import math
 def arithemtic_mean(data):
     #print("data type", type(data))
     return sum(data) / len(data)
+
+def median(data):
+    buff_data = data
+    
+    buff_data.sort()
+
+    print("buf data = ", buff_data)
+
+    if(len(buff_data) % 2 == 0):
+        return (buff_data[math.floor(len( buff_data) / 2) - 1] +  buff_data[math.floor(len(buff_data) / 2)]) / 2
+    if(len(buff_data) % 2 == 1):
+        return  buff_data[math.floor(len(buff_data) / 2)]
+    
+    return None
+
+def Mode(data):
+    buff_array = [0] * len(data) * 10
+
+    for i in range(len(data)):
+        buff_array[data[i] - 1] += 1
+
+    # use dictionary
+    print(buff_array)
+    return None
+
 def test_function():
     print("test")
     
-#if __name__ == "__main__":
-    #pass
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import lib_1 as lib
 
-list = [1, 2]
+list = [1, 2, 4, 5, 5]
 
 print(lib.arithemtic_mean(list))
+
+print(lib.median(list))
+
+
+print(lib.Mode(list))
